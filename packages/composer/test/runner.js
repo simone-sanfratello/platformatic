3856290ed68bdad9e96563ff76fc78d96609aa37
Simone Sanfratello <simone.sanfra@gmail.com>
2023-11-14 11:03:48 +0100

    feat: composer graphql with plt db services

--- packages/composer/test/runner.js
+++ packages/composer/test/runner.js
@@ -2,21 +2,15 @@
 
 const { tap, spec } = require('node:test/reporters')
 const { run } = require('node:test')
-const path = require('node:path')
 const glob = require('glob').globSync
 
 /* eslint-disable new-cap */
 const reporter = process.stdout.isTTY ? new spec() : tap
 
-const files = [
-  ...glob(path.join(__dirname, '*.test.js')),
-  ...glob(path.join(__dirname, 'openapi', '*.test.js')),
-  ...glob(path.join(__dirname, 'telemetry', '*.test.js')),
-  ...glob(path.join(__dirname, 'cli', '*.test.js'))
-]
+const files = glob('test/**/*.test.{js,mjs}')
 
 run({
   files,
-  concurrency: 1,
-  timeout: 30000
+  concurrency: 10,
+  timeout: 30_000
 }).compose(reporter).pipe(process.stdout)
